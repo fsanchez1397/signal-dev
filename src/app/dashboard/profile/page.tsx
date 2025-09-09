ee00929413766c86517164580942b91674336a79
fsanchez1397 <felipesanchez10@protonmail.com>
2025-09-09 03:46:08 -0400

    wip: profile page, added zod

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,11 +1,19 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@radix-ui/react-label";
+import { Label } from "@/components/ui/label";
+import { useActionState } from "react";
 import { onSave } from "./actions";
 export default function () {
+  const initState = {
+    success: false,
+    message: "",
+    errors: [],
+  };
+  const [state, formAction, isPending] = useActionState(onSave, initState);
   return (
     <>
-      <form action={onSave}>
+      <form action={formAction}>
         <Label htmlFor="fullName">Full Name:</Label>
         <Input
           id="fullName"
@@ -15,6 +23,9 @@ export default function () {
           required
         />
         <Button type="submit">Save</Button>
+        {state?.message && (
+          <p className="text-sm text-red-500">{state.message}</p>
+        )}
       </form>
     </>
   );
